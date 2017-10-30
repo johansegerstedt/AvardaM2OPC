28a900a24359a575ca60c91d89968d728b2ce3aa
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-10-30 15:36:46 +0200

    Add M2 browser cache invalidation

--- view/frontend/app/src/root/components/Root.js
+++ view/frontend/app/src/root/components/Root.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import {Provider} from 'react-redux';
+import {get} from 'lodash';
 import CartContainer from '$src/cart/components/CartContainer';
 import type {Actions, AppState} from '../types';
 import type {Store} from 'redux';
@@ -11,6 +12,16 @@ type Props = {
 type State = {};
 
 class Root extends React.Component<Props, State> {
+  clearM2Cache() {
+    if (get(window, 'localStorage')) {
+      localStorage.removeItem('mage-cache-storage');
+    }
+  }
+
+  componentWillMount() {
+    this.clearM2Cache();
+  }
+
   render() {
     const {store} = this.props;
     return (

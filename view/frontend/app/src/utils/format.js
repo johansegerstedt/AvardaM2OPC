4ecab3ff31764af55129e77375a3f96475b654cd
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-12-07 14:32:15 +0200

    Fix currency formatting to support non-numeric values

--- view/frontend/app/src/utils/format.js
+++ view/frontend/app/src/utils/format.js
@@ -1,17 +1,18 @@
 // @flow
 import {getConfig} from '$src/config';
 
+// 'fi_FI' => 'fi-FI' as IETF format is
 const magentoToIETFLanguageTagConverter = (locale: string) =>
   locale.replace(/_/g, '-');
 
 export const formatCurrency = (
-  number: number,
-  currency: string,
+  number: any,
+  currency: string, // currency code eg. 'USD'
   locale?: string = magentoToIETFLanguageTagConverter(
     getConfig().magentoLocale,
   ),
 ): string =>
-  number.toLocaleString(locale, {
+  (typeof number === 'number' ? number : 0).toLocaleString(locale, {
     style: 'currency',
     currency,
   });

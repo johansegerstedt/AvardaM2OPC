f32a8090f8ce74b10dd97667f6150da7a52ca066
Ville Vaarala <ville.l.vaarala@gmail.com>
2018-01-30 11:12:25 +0200

    Unset isFetching flag when fetching purchase ID fails
    AVARDA-11

--- view/frontend/app/src/avarda/constants.js
+++ view/frontend/app/src/avarda/constants.js
@@ -3,6 +3,8 @@
 export const GET_PURCHASE_ID: 'avarda/getPurchaseId' = 'avarda/getPurchaseId';
 export const RECEIVE_PURCHASE_ID: 'avarda/receivePurchaseId' =
   'avarda/receivePurchaseId';
+export const GET_PURCHASE_ID_FAILURE: 'avarda/getPurchaseIdFailure' =
+  'avarda/getPurchaseIdFailure';
 export const ADDRESS_CHANGED: 'avardaAddressChanged' = 'avardaAddressChanged';
 export const COMPLETE_PAYMENT_PRESSED: 'avarda/completePaymentPressed' =
   'avarda/completePaymentPressed';
@@ -12,6 +14,7 @@ export const ActionTypes = {
   COMPLETE_PAYMENT_PRESSED,
   ADDRESS_CHANGED,
   GET_PURCHASE_ID,
+  GET_PURCHASE_ID_FAILURE,
   RECEIVE_PURCHASE_ID,
   UPDATED_ITEMS,
 };

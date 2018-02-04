7c16214297ef455bae676ddd2aac6676b728b49b
Mattias Nixell <mattias.nixell@digia.com>
2018-02-05 00:21:21 +0200

    Fix phpdoc in automatic invoicing

--- Observer/AutomaticInvoicing.php
+++ Observer/AutomaticInvoicing.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * @author      Digia Commerce Oy
+ * @copyright   Copyright © 2017 Digia. All rights reserved.
+ * @package     Digia_AvardaCheckout
  */
 namespace Digia\AvardaCheckout\Observer;
 

6d98784ada1596bcb1019bd0a88f1e481580c700
Mattias Nixell <mattias.nixell@digia.com>
2018-03-12 00:27:24 +0200

    Add exception for not found masked quote ID

--- Model/GuestPaymentManagement.php
+++ Model/GuestPaymentManagement.php
@@ -102,6 +102,13 @@ class GuestPaymentManagement implements GuestPaymentManagementInterface
         $quoteIdMask = $this->quoteIdMaskFactory->create()
             ->load($cartId, 'masked_id');
 
-        return $quoteIdMask->getQuoteId();
+        $quoteId = $quoteIdMask->getData('quote_id');
+        if ($quoteId === null) {
+            throw new \Magento\Framework\Exception\PaymentException(
+                __('Could not find quote with given ID.')
+            );
+        }
+
+        return $quoteId;
     }
 }

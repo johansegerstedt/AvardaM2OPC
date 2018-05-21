6e1693dd09d413844301d229175a8f4cd9fac21d
Daniel Maricic <daniel@woss.io>
2018-05-21 12:04:38 +0200

    cc for app

--- view/frontend/app/src/app/components/App.js
+++ view/frontend/app/src/app/components/App.js
@@ -1,7 +1,6 @@
 // @flow
 import React, {Fragment, Component} from 'react';
 import queryString from 'query-string';
-import {bindActionCreators} from 'redux';
 import {connect, type MapStateToProps} from 'react-redux';
 import {compose} from 'redux';
 import {$, interpolateHTML} from '$i18n';

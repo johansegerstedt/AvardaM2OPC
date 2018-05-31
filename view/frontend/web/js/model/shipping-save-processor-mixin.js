4080ac1c8386512415dc9905c42aeeaafbf55dd7
Daniel Maricic <daniel@woss.io>
2018-05-31 18:32:51 +0200

    lint

--- view/frontend/web/js/model/shipping-save-processor-mixin.js
+++ view/frontend/web/js/model/shipping-save-processor-mixin.js
@@ -1,14 +1,14 @@
 define([
-  'jquery',
-  'ko',
-  'Magento_Checkout/js/model/quote',
-  'Magento_Checkout/js/model/resource-url-manager',
-  'mage/storage',
-  'Magento_Checkout/js/model/payment-service',
-  'Magento_Checkout/js/model/payment/method-converter',
-  'Magento_Checkout/js/model/error-processor',
-  'Magento_Checkout/js/model/full-screen-loader',
-  'Magento_Checkout/js/action/select-billing-address',
+  "jquery",
+  "ko",
+  "Magento_Checkout/js/model/quote",
+  "Magento_Checkout/js/model/resource-url-manager",
+  "mage/storage",
+  "Magento_Checkout/js/model/payment-service",
+  "Magento_Checkout/js/model/payment/method-converter",
+  "Magento_Checkout/js/model/error-processor",
+  "Magento_Checkout/js/model/full-screen-loader",
+  "Magento_Checkout/js/action/select-billing-address"
 ], function(
   $,
   ko,
@@ -21,10 +21,10 @@ define([
   fullScreenLoader,
   selectBillingAddressAction
 ) {
-  'use strict';
+  "use strict";
 
   var mixin = {
-    saveShippingInformation: function () {
+    saveShippingInformation: function() {
       var payload;
 
       // Don't force billing address at this point
@@ -33,37 +33,36 @@ define([
       }*/
 
       payload = {
-          addressInformation: {
-              'shipping_address': quote.shippingAddress(),
-              // 'billing_address': quote.billingAddress(),
-              'shipping_method_code': quote.shippingMethod()['method_code'],
-              'shipping_carrier_code': quote.shippingMethod()['carrier_code']
-          }
+        addressInformation: {
+          shipping_address: quote.shippingAddress(),
+          // 'billing_address': quote.billingAddress(),
+          shipping_method_code: quote.shippingMethod()["method_code"],
+          shipping_carrier_code: quote.shippingMethod()["carrier_code"]
+        }
       };
 
       fullScreenLoader.startLoader();
 
-      return storage.post(
+      return storage
+        .post(
           resourceUrlManager.getUrlForSetShippingInformation(quote),
           JSON.stringify(payload)
-      ).done(
-          function (response) {
-              quote.setTotals(response.totals);
-              paymentService.setPaymentMethods(methodConverter(response['payment_methods']));
-              fullScreenLoader.stopLoader();
-          }
-      ).fail(
-          function (response) {
-              errorProcessor.process(response);
-              fullScreenLoader.stopLoader();
-          }
-      );
+        )
+        .done(function(response) {
+          quote.setTotals(response.totals);
+          paymentService.setPaymentMethods(
+            methodConverter(response["payment_methods"])
+          );
+          fullScreenLoader.stopLoader();
+        })
+        .fail(function(response) {
+          errorProcessor.process(response);
+          fullScreenLoader.stopLoader();
+        });
     }
   };
 
   return function(shippingSaveProcessor) {
     return $.extend({}, shippingSaveProcessor, mixin);
-  }
-
-
-})
+  };
+});
